7f3352ec0219d42057d2fd3557d07669a4845356
Ruvick <rudikov.web@ya.ru>
2024-07-13 18:03:21 +0300

    Структура + Стили

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+</script>
+
+<template>
+
+	<footer class="footer">
+		<div class="footer__container">
+			Footer
+		</div>
+	</footer>
+
+</template>
+
+<style lang="scss" scoped>
+
+
+</style>
